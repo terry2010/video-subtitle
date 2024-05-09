3a971924db8db453ec12efe6822a72fa526403af
terry <jstel@126.com>
2024-05-09 23:08:26 +0800

    手工修复代码里的问题

--- download_model.py
+++ download_model.py
@@ -52,6 +52,8 @@ for model_name in model_list:
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=model_dir)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name, cache_dir=model_dir)
 
+#ct2-transformers-converter --model facebook/nllb-200-3.3B --output_dir ~/tmp-ct2fast-nllb-200-3.3B --force --copy_files tokenizer.json README.md tokenizer_config.json generation_config.json special_tokens_map.json .gitattributes --quantization int8_float16 --trust_remote_code
+
 
 
 # 设置环境变量 WHISPER_MODELS_DIR 为自定义目录
